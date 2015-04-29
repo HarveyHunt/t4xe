91e7954a8e7758cd33d0339cde785a068b3eb7ad
Harvey Hunt <harveyhuntnexus@gmail.com>
2015-04-29 04:25:01 +0100

    Split a unit test into two smaller ones

--- taxe/core/src/test/ResourceManagerTest.java
+++ taxe/core/src/test/ResourceManagerTest.java
@@ -20,22 +20,28 @@ public class ResourceManagerTest {
     }
 
     @Test
-    public void testAddRandomResourceToPlayer() throws Exception {
+    public void testAddRandomResourceToPlayer() {
         PlayerManager playerManager = new PlayerManager();
-        playerManager.createPlayers(2);
+        playerManager.createPlayers(1);
         Player player1 = playerManager.getActivePlayer();
 
-        for (int i=0; i<3; i++){
+        for (int i = 0; i < rm.CONFIG_MAX_RESOURCES - 1; i++){
             rm.addRandomResourceToPlayer(player1);
         }
 
-        Assert.assertEquals(3, player1.getResources().size());
+        Assert.assertEquals(rm.CONFIG_MAX_RESOURCES - 1, player1.getResources().size());
+    }
+
+    @Test
+    public void testAddTooManyRandomResourceToPlayer() {
+        PlayerManager playerManager = new PlayerManager();
+        playerManager.createPlayers(1);
+        Player player1 = playerManager.getActivePlayer();
 
         for (int i = 0; i < rm.CONFIG_MAX_RESOURCES + 5; i++){
             rm.addRandomResourceToPlayer(player1);
         }
 
         Assert.assertEquals(rm.CONFIG_MAX_RESOURCES, player1.getResources().size());
-
     }
 }
